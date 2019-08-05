aae60f8fc737ed1f97c1b94b53fecd4c225ca2b3
RealWillBrooks <wbrooks107@gmail.com>
2019-08-05 16:20:20 -0500

    Finished TodoForm,

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,40 @@
+
+import React from 'react'
+
+class TodoForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            item: ''
+        }
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ item: ''});
+        this.props.addItem(event, this.state.item)
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                
+                <input 
+                    placeholder="What's on your agenda?"
+                    type="text"
+                    name="item"
+                    value={this.state.item}
+                    onChange={this.handleChange}
+                />
+
+                <button onClick={this.handleSubmit}>Add Todo</button>
+                <button>Clear Completed</button>
+
+            </form>
+        )        
+    }
+}
+
+export default TodoForm;
